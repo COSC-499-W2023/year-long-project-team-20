ea64a25bedbc27f2ed34fb81b36480f54a1e0630
mohdsm <mohammed200223@gmail.com>
2023-10-28 13:09:16 -0700

    front page

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,42 @@
-import logo from "./logo.svg";
+import React from 'react';
 import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import { withAuthenticator, Button, View } from "@aws-amplify/ui-react";
+import { Auth } from 'aws-amplify';
 
-function App({ signOut }) {
+const VideoPlayer = () => {
+  return (
+    <div className="video-player">
+      {/* Your video player here */}
+      <h2>Video Player</h2>
+    </div>
+  );
+};
+
+const VideoList = () => {
+  return (
+    <div className="video-list">
+      {/* List of videos here */}
+      <h2>Video List</h2>
+    </div>
+  );
+};
+
+function App() {
+  const signOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('Error signing out: ', error);
+    }
+  };
+
   return (
     <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
       <Button onClick={signOut}>Sign Out</Button>
+      <div className="main-container">
+        <VideoPlayer />
+        <VideoList />
+      </div>
     </View>
   );
 }
