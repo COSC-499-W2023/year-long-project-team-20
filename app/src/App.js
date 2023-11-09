9e6cdfa1b97147889d9d873f537f97c0b5b63e41
Mayaviri7 <66889922+Mayaviri7@users.noreply.github.com>
2023-11-09 13:10:25 -0800

    added amplify figma components

--- app/src/App.js
+++ app/src/App.js
@@ -10,8 +10,15 @@ import {
   View,
   Card,
   Text,
-  Flex
+  Flex,
+  AmplifyProvider
 } from "@aws-amplify/ui-react";
+
+import {
+  
+  NavBarHeader2 
+ } from './ui-components';
+
 
 function App({ signOut }) {
   const [user, setUser] = useState(null);
@@ -23,20 +30,28 @@ function App({ signOut }) {
   }, []);
 
   return (
-    <View padding="all" className="App">
-      <Card>
-        <Flex direction="column" align="center" gap="1rem">
-          <Heading level={1}>Profile</Heading>
-          {user && (
-            <>
-              <Text variant="h2">Username: {user.username}</Text>
-              <Text variant="h2">Email: {user.attributes.email}</Text>
-            </>
-          )}
-          <Button onClick={signOut} variant="brand">Sign Out</Button>
-        </Flex>
-      </Card>
-    </View>
+    <AmplifyProvider>
+      
+ 
+     <View padding="all" className="App">
+       <Card>
+         <Flex direction="column" align="center" gap="1rem">
+         <NavBarHeader2 />
+       
+           <Heading level={1}>Profile</Heading>
+           {user && (
+             <>
+               <Text variant="h2">Username: {user.username}</Text>
+               <Text variant="h2">Email: {user.attributes.email}</Text>
+               
+             </>
+           )}
+           <Button onClick={signOut} variant="brand">Sign Out</Button>
+         </Flex>
+       </Card>
+     </View>
+
+    </AmplifyProvider>
   );
 }
 export default withAuthenticator(App);
