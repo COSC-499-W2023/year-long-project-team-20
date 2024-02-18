3c709ff4ab5bdb57fe4db77d35931b938de04ecf
xIssa11 <issahashim@yahoo.com>
2024-02-17 16:24:30 -0800

    Cypress Test for upload video

---
+++ app/cypress/e2e/upload_video.cy.js
@@ -0,0 +1,44 @@
+
+//test script for testing upload
+describe ('Upload Video',()=>{
+  //logs into test account and navigates to upload page
+  beforeEach(()=>{
+    cy.visit('http://localhost:3000');
+    cy.get('input[name="username"]').type('testuser');
+    cy.get('input[name ="password"]').type('testuser1234');
+    cy.get('button[type="submit"]').click();
+    cy.wait(5000);
+    cy.visit('http://localhost:3000/upload');
+  })
+
+  it('chooses file and uploads a test video', ()=>{
+    
+    // first check if the select file and download button is visible
+    cy.get('[data-testid="uploadVideo"]').should('be.visible');
+    cy.get('[data-testid="chooseFile"]').should('be.visible');
+
+    //simulate upload process
+    cy.fixture('testVideo.mp4').then(fileContent =>{
+      cy.get('[data-testid="chooseFile"]').attachFile({
+        fileContent: fileContent,
+        fileName: 'testVideo.mp4',
+        mimeType: 'video/mp4'
+      })
+    })
+
+    // click upload button
+    cy.get('[data-testid="uploadVideo"]').click();
+
+     // Wait for the console log indicating that upload is completed
+     cy.window().then((win) => {
+      cy.spy(win.console, 'log').as('consoleLog')
+    })
+
+    //
+    cy.get('@consoleLog').should('be.calledWith', 'Successfully uploaded video') 
+    // this concludes the test for now
+    // test could be expanded to by navigating to the upload page and checking to see if the video that was uploaded is present
+
+
+  })
+})
