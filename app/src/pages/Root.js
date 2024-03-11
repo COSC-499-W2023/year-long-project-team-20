e54893f3cdc8a92818ec1fda7894599edb2a926a
xIssa11 <issahashim@yahoo.com>
2024-03-10 17:59:16 -0700

    Logo now directs to home page

--- app/src/pages/Root.js
+++ app/src/pages/Root.js
@@ -1,11 +1,10 @@
-
 import { Outlet } from "react-router-dom";
-import '../css/navbar.css';
+import "../css/navbar.css";
 
 import logo from "../logo.svg";
 import "@aws-amplify/ui-react/styles.css";
-import React, { useEffect, useState } from 'react';
-import { Auth } from 'aws-amplify';
+import React, { useEffect, useState } from "react";
+import { Auth } from "aws-amplify";
 import {
   withAuthenticator,
   Button,
@@ -15,117 +14,108 @@ import {
   Card,
   Text,
   Flex,
-  AmplifyProvider
+  AmplifyProvider,
 } from "@aws-amplify/ui-react";
 
-import {
-  Mynavbar 
- } from '../ui-components';
- import { useNavigate } from 'react-router-dom';
- 
+import { Mynavbar } from "../ui-components";
+import { useNavigate } from "react-router-dom";
 
- //Components that are in root will show up in all pages of our website, such as the navigation bar
+//Components that are in root will show up in all pages of our website, such as the navigation bar
 const Root = ({ signOut }) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-      Auth.currentAuthenticatedUser()
-        .then(user => setUser(user))
-        .catch(err => console.log(err));
-    }, []);
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((user) => setUser(user))
+      .catch((err) => console.log(err));
+  }, []);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    //Override amplify component so that nav bar buttons lead to the correct page
-    const navbarOverrides = {
-        "Home" : {
-          style: {
-            cursor: "pointer",
-          },
-    
-          onClick: () => {
-            navigate("/");
-          }
-        },
-    
-        "profile" : {
-          style: {
-            cursor: "pointer",
-          },
-    
-          onClick: () => {
-            navigate("/profile");
-          }
-        },
-    
-        "Record" : {
-            style: {
-              cursor: "pointer",
-            },
-      
-            onClick: () => {
-              navigate("/record");
-            }
-          },
+  //Override amplify component so that nav bar buttons lead to the correct page
+  const navbarOverrides = {
+    Home: {
+      style: {
+        cursor: "pointer",
+      },
 
-          "Upload" : {
-            style: {
-              cursor: "pointer",
-            },
-      
-            onClick: () => {
-              navigate("/upload");
-            }
-          },
+      onClick: () => {
+        navigate("/");
+      },
+    },
 
-          "Library" : {
-            style: {
-              cursor: "pointer",
-            },
-      
-            onClick: () => {
-              navigate("/library");
-            }
-          },
-        
-        "About" : {
-          style: {
-            cursor: "pointer",
-          },
-    
-          onClick: () => {
-            navigate("/About");
-          }
-        },
-        "Chat" : {
-          style: {
-            cursor: "pointer",
-          },
-    
-          onClick: () => {
-            navigate("/Chat");
-          }
-        }
-    
-      }
+    profile: {
+      style: {
+        cursor: "pointer",
+      },
+
+      onClick: () => {
+        navigate("/profile");
+      },
+    },
+
+    Record: {
+      style: {
+        cursor: "pointer",
+      },
+
+      onClick: () => {
+        navigate("/record");
+      },
+    },
+
+    Upload: {
+      style: {
+        cursor: "pointer",
+      },
+
+      onClick: () => {
+        navigate("/upload");
+      },
+    },
+
+    Library: {
+      style: {
+        cursor: "pointer",
+      },
+
+      onClick: () => {
+        navigate("/library");
+      },
+    },
+    logo: {
+      style: {
+        cursor: "pointer",
+      },
+
+      onClick: () => {
+        navigate("/About");
+      },
+    },
+
+    About: {
+      style: {
+        cursor: "pointer",
+      },
+
+      onClick: () => {
+        navigate("/");
+      },
+    },
+  };
 
   return (
     <AmplifyProvider>
-     <View padding="all" className="App">
-       <Card>
-         <Flex direction="column" align="center" gap="1rem">
-         <Mynavbar width={"100"}
-          overrides={navbarOverrides} 
-         /> 
-         <Outlet></Outlet>
-         </Flex>
-       </Card>
-     </View>
-
+      <View padding="all" className="App">
+        <Card>
+          <Flex direction="column" align="center" gap="1rem">
+            <Mynavbar width={"100"} overrides={navbarOverrides} />
+            <Outlet></Outlet>
+          </Flex>
+        </Card>
+      </View>
     </AmplifyProvider>
   );
-
-  
 };
 
-export default withAuthenticator (Root);
+export default withAuthenticator(Root);
